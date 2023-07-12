12210d93c22f7027947c3591e5e2e879de7c355b
Paul Miller <paul@paulmillr.com>
2023-07-12 21:58:14 +0000

    Lint

--- src/utils.ts
+++ src/utils.ts
@@ -21,7 +21,9 @@ export const createView = (arr: TypedArray) =>
 export const isLE = new Uint8Array(new Uint32Array([0x11223344]).buffer)[0] === 0x44;
 if (!isLE) throw new Error('Non little-endian hardware is not supported');
 
-const hexes = /* @__PURE__ */ Array.from({ length: 256 }, (_, i) => i.toString(16).padStart(2, '0'));
+const hexes = /* @__PURE__ */ Array.from({ length: 256 }, (_, i) =>
+  i.toString(16).padStart(2, '0')
+);
 /**
  * @example bytesToHex(Uint8Array.from([0xca, 0xfe, 0x01, 0x23])) // 'cafe0123'
  */
