ccd6c818cbc847b7ec31b5e9cd38ea7f44b1cd92
Paul Miller <paul@paulmillr.com>
2023-07-30 17:06:36 +0000

    build: fix

--- build/input.js
+++ build/input.js
@@ -1,7 +1,7 @@
 export {
-  salsa20, xsalsa20_poly1305,
-  chacha20, chacha8, chacha12,
-  chacha20_poly1305, xchacha20_poly1305,
-} from '@noble/ciphers/_slow';
+  xsalsa20poly1305, chacha20poly1305, xchacha20poly1305,
+  salsa20, chacha20, chacha8, chacha12,
+} from '@noble/ciphers/_micro';
+// export { xsalsa20poly1305 } from '@noble/ciphers/salsa';
 import { bytesToHex, bytesToUtf8, hexToBytes, utf8ToBytes } from '@noble/ciphers/utils';
 export const utils = { bytesToHex, bytesToUtf8, hexToBytes, utf8ToBytes };
