1b7f3db7abf6f79eef771c96ec9e557b992ade75
Paul Miller <paul@paulmillr.com>
2023-10-18 20:35:17 +0200

    Fix webcrypto

--- src/webcrypto/utils.ts
+++ src/webcrypto/utils.ts
@@ -4,7 +4,7 @@
 // from `crypto` to `cryptoNode`, which imports native module.
 // Makes the utils un-importable in browsers without a bundler.
 // Once node.js 18 is deprecated, we can just drop the import.
-import { crypto } from './crypto.js';
+import { crypto } from '@noble/ciphers/webcrypto/crypto';
 import { Cipher, concatBytes } from '../utils.js';
 import { number } from '../_assert.js';
 
