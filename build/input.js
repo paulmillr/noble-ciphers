d771ec03b144e5131cf5a9944874ecfc54198183
Paul Miller <paul@paulmillr.com>
2024-02-16 22:24:48 +0100

    Fix build import

--- build/input.js
+++ build/input.js
@@ -3,6 +3,6 @@ export {
   salsa20, chacha20, chacha8, chacha12,
 } from '@noble/ciphers/_micro';
 export { ecb, ctr, cbc, gcm, siv } from '@noble/ciphers/aes';
-import { randomBytes } from '@noble/ciphers/webcrypto/utils';
+import { randomBytes } from '@noble/ciphers/webcrypto';
 import { bytesToHex, bytesToUtf8, hexToBytes, utf8ToBytes } from '@noble/ciphers/utils';
 export const utils = { bytesToHex, bytesToUtf8, hexToBytes, randomBytes, utf8ToBytes };
