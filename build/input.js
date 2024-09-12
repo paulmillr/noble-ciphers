dced50f04d2de8152e42cea4a104df6dabaf4d3f
Paul Miller <paul@paulmillr.com>
2024-09-12 23:23:15 +0000

    build: add aeskw

--- build/input.js
+++ build/input.js
@@ -2,7 +2,7 @@ export {
   xsalsa20poly1305, chacha20poly1305, xchacha20poly1305,
   salsa20, chacha20, chacha8, chacha12,
 } from '@noble/ciphers/_micro';
-export { ecb, ctr, cbc, gcm, siv } from '@noble/ciphers/aes';
+export { ecb, ctr, cbc, gcm, siv, aeskw, aeskwp } from '@noble/ciphers/aes';
 import { randomBytes } from '@noble/ciphers/webcrypto';
 import { bytesToHex, bytesToUtf8, hexToBytes, utf8ToBytes } from '@noble/ciphers/utils';
 export const utils = { bytesToHex, bytesToUtf8, hexToBytes, randomBytes, utf8ToBytes };
