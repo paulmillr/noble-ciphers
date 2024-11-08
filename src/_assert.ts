0a7bbfce6261e3933097c8f49c2a7bf71351e11f
Paul Miller <paul@paulmillr.com>
2024-11-08 15:54:23 +0000

    Speed-up byte array checks.

--- src/_assert.ts
+++ src/_assert.ts
@@ -7,10 +7,7 @@ function bool(b: boolean) {
 }
 
 export function isBytes(a: unknown): a is Uint8Array {
-  return (
-    a instanceof Uint8Array ||
-    (a != null && typeof a === 'object' && a.constructor.name === 'Uint8Array')
-  );
+  return a instanceof Uint8Array || (ArrayBuffer.isView(a) && a.constructor.name === 'Uint8Array');
 }
 
 function bytes(b: Uint8Array | undefined, ...lengths: number[]) {
