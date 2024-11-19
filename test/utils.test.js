780811eaa72029bf41360bbe5b7b18705c496eb2
Paul Miller <paul@paulmillr.com>
2024-11-19 07:43:04 +0000

    test: use package hex methods, instead of nodejs

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,13 +1,28 @@
 const { deepStrictEqual, throws } = require('assert');
 const { describe, should } = require('micro-should');
-const utils = require('./utils.js');
+const { TYPE_TEST, unalign } = require('./utils.js');
+const utils = require('../utils.js');
 
 // Here goes test for tests...
 describe('Tests', () => {
-  should('Unalign', () => {
+  should('hexToBytes', () => {
+    for (let v of TYPE_TEST.hex) {
+      throws(() => {
+        utils.hexToBytes(v);
+      });
+    }
+  });
+  should('bytesToHex', () => {
+    for (let v of TYPE_TEST.bytes) {
+      throws(() => {
+        utils.bytesToHex(v);
+      });
+    }
+  });
+  should('unalign', () => {
     const arr = new Uint8Array([1, 2, 3]);
     for (let i = 0; i < 16; i++) {
-      const tmp = utils.unalign(arr, i);
+      const tmp = unalign(arr, i);
       deepStrictEqual(tmp, arr);
       deepStrictEqual(tmp.byteOffset, i);
       // check that it doesn't modify original
