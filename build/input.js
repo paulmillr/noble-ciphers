5d135156f12ec7484d071a1fd29fda24b0e35859
Paul Miller <paul@paulmillr.com>
2024-11-22 17:49:23 +0000

    build: use non-micro

--- build/input.js
+++ build/input.js
@@ -1,7 +1,7 @@
+export { xsalsa20poly1305, salsa20 } from '@noble/ciphers/salsa';
 export {
-  xsalsa20poly1305, chacha20poly1305, xchacha20poly1305,
-  salsa20, chacha20, chacha8, chacha12,
-} from '@noble/ciphers/_micro';
+  chacha20poly1305, xchacha20poly1305, chacha20, chacha8, chacha12,
+} from '@noble/ciphers/chacha';
 export { ecb, ctr, cbc, gcm, siv, aeskw, aeskwp } from '@noble/ciphers/aes';
 import { randomBytes } from '@noble/ciphers/webcrypto';
 import { bytesToHex, bytesToUtf8, hexToBytes, utf8ToBytes } from '@noble/ciphers/utils';
