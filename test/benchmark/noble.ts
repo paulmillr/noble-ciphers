acb5d03b86a78fd1ea508084299569d8b42334d0
Paul Miller <paul@paulmillr.com>
2025-05-29 14:43:22 +0000

    Update micro-bmark to auto-set samples

--- test/benchmark/noble.ts
+++ test/benchmark/noble.ts
@@ -13,12 +13,12 @@ import * as aesw from '../../src/webcrypto.ts';
 import { buf } from './_utils.ts';
 
 const buffers = [
-  // { size: '16B', samples: 1_500_000, data: buf(16) }, // common block size
-  // { size: '32B', samples: 1_500_000, data: buf(32) },
-  { size: '64B', samples: 500_000, data: buf(64) },
-  // { size: '1KB', samples: 50_000, data: buf(1024) },
-  // { size: '8KB', samples: 10_000, data: buf(1024 * 8) },
-  { size: '1MB', samples: 100, data: buf(1024 * 1024) },
+  // { size: '16B', data: buf(16) }, // common block size
+  // { size: '32B', data: buf(32) },
+  { size: '64B', data: buf(64) },
+  // { size: '1KB', data: buf(1024) },
+  // { size: '8KB', data: buf(1024 * 8) },
+  { size: '1MB', data: buf(1024 * 1024) },
 ];
 
 async function main() {
@@ -29,24 +29,24 @@ async function main() {
   const nonce24 = buf(24);
   // Do we need this at all?
   for (let i = 0; i < 100_000; i++) xsalsa20poly1305(key, nonce24).encrypt(buf(64)); // warm-up
-  for (const { size, samples: i, data: buf } of buffers) {
+  for (const { size, data: buf } of buffers) {
     console.log(size);
-    await mark('xsalsa20poly1305', i, () => xsalsa20poly1305(key, nonce24).encrypt(buf));
-    await mark('chacha20poly1305', i, () => chacha20poly1305(key, nonce).encrypt(buf));
-    await mark('xchacha20poly1305', i, () => xchacha20poly1305(key, nonce24).encrypt(buf));
-    await mark('aes-256-gcm', i, () => gcm(key, nonce).encrypt(buf));
-    await mark('aes-256-gcm-siv', i, () => gcmsiv(key, nonce).encrypt(buf));
+    await mark('xsalsa20poly1305', () => xsalsa20poly1305(key, nonce24).encrypt(buf));
+    await mark('chacha20poly1305', () => chacha20poly1305(key, nonce).encrypt(buf));
+    await mark('xchacha20poly1305', () => xchacha20poly1305(key, nonce24).encrypt(buf));
+    await mark('aes-256-gcm', () => gcm(key, nonce).encrypt(buf));
+    await mark('aes-256-gcm-siv', () => gcmsiv(key, nonce).encrypt(buf));
 
     console.log('# Unauthenticated encryption');
-    await mark('salsa20', i, () => salsa20(key, nonce8, buf));
-    await mark('xsalsa20', i, () => xsalsa20(key, nonce24, buf));
-    await mark('chacha20', i, () => chacha20(key, nonce, buf));
-    await mark('xchacha20', i, () => xchacha20(key, nonce24, buf));
-    await mark('chacha8', i, () => chacha8(key, nonce, buf));
-    await mark('chacha12', i, () => chacha12(key, nonce, buf));
-    await mark('aes-ecb-256', i, () => ecb(key).encrypt(buf));
-    await mark('aes-cbc-256', i, () => cbc(key, nonce16).encrypt(buf));
-    await mark('aes-ctr-256', i, () => ctr(key, nonce16).encrypt(buf));
+    await mark('salsa20', () => salsa20(key, nonce8, buf));
+    await mark('xsalsa20', () => xsalsa20(key, nonce24, buf));
+    await mark('chacha20', () => chacha20(key, nonce, buf));
+    await mark('xchacha20', () => xchacha20(key, nonce24, buf));
+    await mark('chacha8', () => chacha8(key, nonce, buf));
+    await mark('chacha12', () => chacha12(key, nonce, buf));
+    await mark('aes-ecb-256', () => ecb(key).encrypt(buf));
+    await mark('aes-cbc-256', () => cbc(key, nonce16).encrypt(buf));
+    await mark('aes-ctr-256', () => ctr(key, nonce16).encrypt(buf));
 
     if (size === '1MB') {
       console.log('# Wrapper over built-in webcrypto');
