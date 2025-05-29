60391df2f5e41f087a49bb8dc6cf07918d5f3b78
Paul Miller <paul@paulmillr.com>
2025-05-29 14:58:31 +0000

    Release 2.0.0-beta.1

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,24 @@
  * Audited & minimal JS implementation of Salsa20, ChaCha and AES. Check out individual modules.
  * @example
 ```js
-import { gcm, siv } from '@noble/ciphers/aes';
-import { xsalsa20poly1305 } from '@noble/ciphers/salsa';
-import { secretbox } from '@noble/ciphers/salsa'; // == xsalsa20poly1305
-import { chacha20poly1305, xchacha20poly1305 } from '@noble/ciphers/chacha';
+import { gcm, siv } from '@noble/ciphers/aes.js';
+import { xsalsa20poly1305 } from '@noble/ciphers/salsa.js';
+import { secretbox } from '@noble/ciphers/salsa.js'; // == xsalsa20poly1305
+import { chacha20poly1305, xchacha20poly1305 } from '@noble/ciphers/chacha.js';
 
 // Unauthenticated encryption: make sure to use HMAC or similar
-import { ctr, cfb, cbc, ecb } from '@noble/ciphers/aes';
-import { salsa20, xsalsa20 } from '@noble/ciphers/salsa';
-import { chacha20, xchacha20, chacha8, chacha12 } from '@noble/ciphers/chacha';
+import { ctr, cfb, cbc, ecb } from '@noble/ciphers/aes.js';
+import { salsa20, xsalsa20 } from '@noble/ciphers/salsa.js';
+import { chacha20, xchacha20, chacha8, chacha12 } from '@noble/ciphers/chacha.js';
 
 // KW
-import { aeskw, aeskwp } from '@noble/ciphers/aes';
+import { aeskw, aeskwp } from '@noble/ciphers/aes.js';
 
 // Utilities
-import { bytesToHex, hexToBytes, bytesToUtf8, utf8ToBytes } from '@noble/ciphers/utils';
-import { managedNonce, randomBytes } from '@noble/ciphers/webcrypto';
-import { poly1305 } from '@noble/ciphers/_poly1305';
-import { ghash, polyval } from '@noble/ciphers/_polyval';
+import { bytesToHex, hexToBytes, bytesToUtf8, utf8ToBytes } from '@noble/ciphers/utils.js';
+import { managedNonce, randomBytes } from '@noble/ciphers/webcrypto.js';
+import { poly1305 } from '@noble/ciphers/_poly1305.js';
+import { ghash, polyval } from '@noble/ciphers/_polyval.js';
 ```
  * @module
  */
